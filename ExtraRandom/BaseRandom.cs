684696bad108822700a665045feecc0494a62900
Ruud Schouten <ruud.schouten@fourtress.nl>
2022-08-26 11:21:06 +0200

    Added a `Reseed` method to `BaseRandom`, and removed a method from all return values

--- ExtraRandom/BaseRandom.cs
+++ ExtraRandom/BaseRandom.cs
@@ -15,22 +15,20 @@ public abstract class BaseRandom
     }
 
     /// <summary>
+    /// Reseed the psuedo-random number generation.
     /// </summary>
-    /// <summary>
-    /// Generate a random <see cref="int"/> value.
-    /// </summary>
-    /// <returns>The generated number.</returns>
-    public int NextInt()
-    {
-        return NextInt(0, int.MaxValue);
-    }
+    /// <param name="seed">new seed to use for generation.</param>
+    public abstract void Reseed(int seed);
 
     /// <summary>
     /// Generate a random <see cref="int"/> value where the maximum value is specified.
     /// </summary>
-    /// <param name="max">The maximum value the generated number can be.</param>
+    /// <param name="max">
+    /// The maximum value the generated number can be.
+    /// <para><b>Default</b>: int.<see cref="int.MaxValue"/>.</para>
+    /// </param>
     /// <returns>The generated number.</returns>
-    public int NextInt(int max)
+    public int NextInt(int max = int.MaxValue)
     {
         return NextInt(0, max);
     }
@@ -44,20 +42,14 @@ public abstract class BaseRandom
     public abstract int NextInt(int min, int max);
 
     /// <summary>
-    /// Generate a random <see cref="float"/> value.
-    /// </summary>
-    /// <returns>The generated number.</returns>
-    public float NextFloat()
-    {
-        return NextFloat(0, float.MaxValue);
-    }
-
-    /// <summary>
     /// Generate a random <see cref="float"/> value where the maximum value is specified.
     /// </summary>
-    /// <param name="max">The maximum value the generated number can be.</param>
+    /// <param name="max">
+    /// The maximum value the generated number can be.
+    /// <para><b>Default</b>: 1f.</para>
+    /// </param>
     /// <returns>The generated number.</returns>
-    public float NextFloat(float max)
+    public float NextFloat(float max = 1f)
     {
         return NextFloat(0, max);
     }
@@ -71,20 +63,14 @@ public abstract class BaseRandom
     public abstract float NextFloat(float min, float max);
 
     /// <summary>
-    /// Generate a random <see cref="long"/> value.
-    /// </summary>
-    /// <returns>The generated number.</returns>
-    public long NextLong()
-    {
-        return NextLong(0, long.MaxValue);
-    }
-
-    /// <summary>
     /// Generate a random <see cref="long"/> value where the maximum value is specified.
     /// </summary>
-    /// <param name="max">The maximum value the generated number can be.</param>
+    /// <param name="max">
+    /// The maximum value the generated number can be.
+    /// <para><b>Default</b>: long.<see cref="long.MaxValue"/>.</para>
+    /// </param>
     /// <returns>The generated number.</returns>
-    public long NextLong(long max)
+    public long NextLong(long max = long.MaxValue)
     {
         return NextLong(0, max);
     }
@@ -98,20 +84,14 @@ public abstract class BaseRandom
     public abstract long NextLong(long min, long max);
 
     /// <summary>
-    /// Generate a random <see cref="double"/> value.
-    /// </summary>
-    /// <returns>The generated number.</returns>
-    public double NextDouble()
-    {
-        return NextDouble(0, double.MaxValue);
-    }
-
-    /// <summary>
     /// Generate a random <see cref="double"/> value where the maximum value is specified.
     /// </summary>
-    /// <param name="max">The maximum value the generated number can be.</param>
+    /// <param name="max">
+    /// The maximum value the generated number can be.
+    /// <para><b>Default</b>: 1.0.</para>
+    /// </param>
     /// <returns>The generated number.</returns>
-    public double NextDouble(double max)
+    public double NextDouble(double max = 1.0)
     {
         return NextDouble(0, max);
     }
--- ExtraRandom/RegularRandom.cs
+++ ExtraRandom/RegularRandom.cs
@@ -1,45 +1,65 @@
 ﻿namespace ExtraRandom;
 
+using System.Diagnostics.CodeAnalysis;
+
+/// <summary>
+/// A basic wrapper class around the regular .NET <see cref="Random"/>.
+/// </summary>
+[SuppressMessage(
+    "Security",
+    "SCS0005:Weak random number generator.",
+    Justification = "Weak number generation is acceptable for this class.")]
 public class RegularRandom : BaseRandom
 {
+    /// <summary>
+    /// The Random Generation Generator by .NET.
+    /// </summary>
+    private Random _random;
+
     /// <summary>
     /// Initializes a new instance of the <see cref="RegularRandom"/> class with a random seed.
     /// </summary>
     public RegularRandom()
     {
+        _random = new Random();
     }
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="RegularRandom"/> class with the given <paramref name="seed"/>
+    /// Initializes a new instance of the <see cref="RegularRandom"/> class with the provided <paramref name="seed"/>.
     /// </summary>
-    /// <param name="seed">The seed that should be used to generate random numbers.</param>
+    /// <param name="seed">seed that should be used to generate random numbers.</param>
     public RegularRandom(int seed)
-        : base(seed)
     {
+        _random = new Random(seed);
+    }
+
+    /// <inheritdoc/>
+    public override void Reseed(int seed)
+    {
+        _random = new Random(seed);
     }
 
     /// <inheritdoc/>
     public override int NextInt(int min, int max)
     {
-        // TODO: Do something with the bits, like https://github.com/Shiroechi/Litdex.Random/blob/main/Source/Random.cs#L227
-        return (int)((ShisuaRng.Next() * (ulong)(max - min)) + (ulong)min);
+        return _random.Next(min, max);
     }
 
     /// <inheritdoc/>
     public override float NextFloat(float min, float max)
     {
-        throw new NotImplementedException();
+        return (_random.NextSingle() * (max - min)) + min;
     }
 
     /// <inheritdoc/>
     public override long NextLong(long min, long max)
     {
-        throw new NotImplementedException();
+        return _random.NextInt64(min, max);
     }
 
     /// <inheritdoc/>
     public override double NextDouble(double min, double max)
     {
-        throw new NotImplementedException();
+        return (_random.NextDouble() * (max - min)) + min;
     }
 }
