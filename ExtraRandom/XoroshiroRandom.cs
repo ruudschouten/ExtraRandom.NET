6adcabd1d14968607d0d3f84d6ac61e6c9560766
Ruud Schouten <ruud.schouten@fourtress.nl>
2022-08-26 11:21:55 +0200

    Implemented `Xoroshiro128+` PRNG, and wrote a wrapper around it, so it can be easily used.

---
+++ ExtraRandom/XoroshiroRandom.cs
@@ -0,0 +1,61 @@
+﻿namespace ExtraRandom;
+
+using ExtraRandom.PRNG;
+
+/// <summary>
+/// A wrapper around the <see cref="Xoroshiro128Plus"/> Psuedo-Random Number Generator."/>
+/// </summary>
+public class XoroshiroRandom : BaseRandom
+{
+    /// <summary>
+    /// A Psuedo-Random Number Generator based on the <a href="https://prng.di.unimi.it/">Xoshiro / Xoroshiro generators</a>.
+    /// </summary>
+    private Xoroshiro128Plus _xoroshiro;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="XoroshiroRandom"/> class with a random seed.
+    /// </summary>
+    public XoroshiroRandom()
+    {
+        _xoroshiro = default;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="XoroshiroRandom"/> class with the given <paramref name="seed"/>
+    /// </summary>
+    /// <param name="seed">The seed that should be used to generate random numbers.</param>
+    public XoroshiroRandom(int seed)
+    {
+        _xoroshiro = new Xoroshiro128Plus(seed);
+    }
+
+    /// <inheritdoc/>
+    public override void Reseed(int seed)
+    {
+        _xoroshiro.Reseed(seed);
+    }
+
+    /// <inheritdoc/>
+    public override int NextInt(int min, int max)
+    {
+        return Convert.ToInt32(NextFloat(min, max));
+    }
+
+    /// <inheritdoc/>
+    public override float NextFloat(float min, float max)
+    {
+        return (_xoroshiro.NextFloat() * (max - min)) + min;
+    }
+
+    /// <inheritdoc/>
+    public override long NextLong(long min, long max)
+    {
+        return Convert.ToInt64(NextDouble(min, max));
+    }
+
+    /// <inheritdoc/>
+    public override double NextDouble(double min, double max)
+    {
+        return (_xoroshiro.NextDouble() * (max - min)) + min;
+    }
+}
