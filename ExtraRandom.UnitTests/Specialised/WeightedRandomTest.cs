1674c0c0a74276e2eb59c979a281aff779f115d2
ruudschouten <ruud.schouten@outlook.com>
2023-10-13 09:41:09 +0200

    Added additional tests primarily for performance stuff

--- ExtraRandom.UnitTests/Specialised/WeightedRandomTest.cs
+++ ExtraRandom.UnitTests/Specialised/WeightedRandomTest.cs
@@ -1,3 +1,4 @@
+using ExtraRandom.PRNG;
 using ExtraRandom.Specialised;
 using ExtraRandom.UnitTests.Util;
 using Xunit.Abstractions;
@@ -23,7 +24,7 @@ public class WeightedRandomTest
     [Theory]
     [ClassData(typeof(PRNGTestData))]
 #pragma warning disable S2699 // There is nothing to really test here, just need to see if it generates stuff.
-    public void WeightedTest(IRandom rand)
+    public void Weighted_Rolls_Test(IRandom rand)
 #pragma warning restore S2699
     {
         const int rolls = 1_000_000;
@@ -61,6 +62,48 @@ public class WeightedRandomTest
         foreach (var valuesToPick in valuesToPicks)
         {
             _output.WriteLine($"{valuesToPick.Key} - {valuesToPick.Value}");
+        }
+    }
+
+    [Theory]
+    [ClassData(typeof(PRNGTestData))]
+#pragma warning disable S2699 // There is nothing to really test here, just need to see if it generates stuff.
+    public void Weighted_Speed_Test(IRandom rand)
+#pragma warning restore S2699
+    {
+        const int rolls = 1_000_000;
+
+        var weightedRandom = new WeightedRandom<string>(rand);
+        weightedRandom.Add("1", 1);
+        weightedRandom.Add("10", 10);
+        weightedRandom.Add("50", 50);
+        weightedRandom.Add("5", 5);
+        weightedRandom.Add("25", 25);
+        weightedRandom.Add("125", 125);
+        weightedRandom.Add("20", 20);
+        weightedRandom.Add("45", 45);
+        weightedRandom.Add("0", 0);
+        weightedRandom.Add("17", 17);
+
+        for (var i = 0; i < rolls; i++)
+        {
+            weightedRandom.Next();
+        }
+    }
+
+    [Fact]
+#pragma warning disable S2699 // There is nothing to really test here, just need to see if it generates stuff.
+    public void Weighted_Adding_Test()
+#pragma warning restore S2699
+    {
+        const int numbersToAdd = 50_000;
+        var random = new RomuDuoJr(500);
+
+        var weightedRandom = new WeightedRandom<string>(random);
+
+        for (var i = 0; i < numbersToAdd; i++)
+        {
+            weightedRandom.Add($"{i}", random.NextByte());
         }
     }
 }
