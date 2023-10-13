38bd1f048815f3ac6cd6178fc4cbda49299cee24
ruudschouten <ruud.schouten@outlook.com>
2023-10-13 15:21:09 +0200

    Did the same for MergeSort

--- ExtraSort/MergeSortExtension.cs
+++ ExtraSort/MergeSortExtension.cs
@@ -1,20 +1,34 @@
 namespace ExtraSort;
 
 /// <summary>
-/// Extension method for <see cref="IList{T}"/> to add MergeSort.
+/// Extension method for <see cref="IList{T}"/> to add Merge Sort.
 /// </summary>
 public static class MergeSortExtension
 {
     /// <summary>
-    /// Perform a MergeSort on the <paramref name="list"/>.
+    /// Perform a Merge Sort on the <paramref name="list"/>.
     /// </summary>
-    /// <param name="list">List to perform the MergeSort on.</param>
+    /// <param name="list">List to perform the Merge Sort on.</param>
     /// <typeparam name="T">Type of elements in the <paramref name="list"/>.</typeparam>
     /// <returns>The <paramref name="list"/> sorted.</returns>
     public static IList<T> MergeSort<T>(this IList<T> list)
         where T : IComparable<T>
     {
         return list.Sort(0, list.Count - 1);
+    }
+
+    /// <summary>
+    /// Perform a partial Merge Sort on a section of the <paramref name="list"/>.
+    /// </summary>
+    /// <param name="list">List to perform the Merge Sort on.</param>
+    /// <param name="start">Starting index for the range.</param>
+    /// <param name="end">Exclusive end index for the range.</param>
+    /// <typeparam name="T">Type of elements in the <paramref name="list"/>.</typeparam>
+    /// <returns>The <paramref name="list"/> sorted.</returns>
+    public static IList<T> MergeSort<T>(this IList<T> list, int start, int end)
+        where T : IComparable<T>
+    {
+        return list.Sort(start, end);
     }
 
     private static T[] Sort<T>(this IList<T> list, int start, int end)
