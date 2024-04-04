db4b449703512f3699510a5a75e0bb12287bad44
ruudschouten <ruud.schouten@outlook.com>
2024-04-04 13:37:16 +0200

    Wrote out first unit test that actually tests random functionality

--- ExtraRandom.UnitTests/RandomTests.cs
+++ ExtraRandom.UnitTests/RandomTests.cs
@@ -1,6 +1,21 @@
-﻿namespace ExtraRandom.UnitTests;
+﻿using ExtraRandom.TestHelper;
+using FluentAssertions;
+
+namespace ExtraRandom.UnitTests;
 
 public class RandomTests
 {
-    
+    [Theory]
+    [ClassData(typeof(PRNGWithSeedsTestData))]
+    public void Generating_NextBoolean_IsTrueOrFalse(IRandom random, ulong seed)
+    {
+        // Arrange
+        random.SetSeed(seed);
+
+        // Act
+        var result = random.NextBoolean();
+
+        // Assert
+        Assert.True(result || !result);
+    }
 }
