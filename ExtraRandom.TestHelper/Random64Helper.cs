899230425b5a0623d4e2d7fb0330f63b78f722b1
ruudschouten <ruud.schouten@outlook.com>
2024-04-11 16:37:47 +0200

    Added helper method to get State from a Random64 using reflection

---
+++ ExtraRandom.TestHelper/Random64Helper.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+namespace ExtraRandom.TestHelper;
+
+public static class Random64Helper
+{
+    public static ulong[] GetStateCopy(this Random64 random)
+    {
+        var state = random.GetType().GetField("State", BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(random) as ulong[];
+        return (state ?? []).ToArray();
+    }
+}
