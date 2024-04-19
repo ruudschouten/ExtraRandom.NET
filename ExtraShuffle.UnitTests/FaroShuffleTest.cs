2a190c54df101452962aa61a5195e2f71195ffad
ruudschouten <ruud.schouten@outlook.com>
2024-04-19 12:25:05 +0200

    Added ExtraShuffle package, with FaroShuffle

---
+++ ExtraShuffle.UnitTests/FaroShuffleTest.cs
@@ -0,0 +1,24 @@
+﻿using ExtraShuffle.Shuffles;
+using FluentAssertions;
+
+namespace ExtraShuffle.UnitTests;
+
+public class FaroShuffleTest
+{
+    private readonly List<int> _list = [1, 2, 3, 4, 5, 6, 7, 8];
+
+    [Fact]
+    public void TestEven()
+    {
+        _list.FaroShuffle();
+        _list.Should().ContainInOrder(1, 5, 2, 6, 3, 7, 4, 8);
+    }
+
+    [Fact]
+    public void TestUneven()
+    {
+        _list.Add(9);
+        _list.FaroShuffle();
+        _list.Should().ContainInOrder(1, 5, 2, 6, 3, 7, 4, 8, 9);
+    }
+}
---
+++ ExtraShuffle/Shuffles/FaroShuffleExtension.cs
@@ -0,0 +1,28 @@
+﻿using ExtraSort;
+
+namespace ExtraShuffle.Shuffles;
+
+public static class FaroShuffleExtension
+{
+    public static void FaroShuffle<T>(this IList<T> list, int min, int max)
+    {
+        var mid = (min + max) / 2;
+        var firstHalf = list.Slice(min, mid - 1).ToArray();
+        var secondHalf = list.Slice(mid, max).ToArray();
+        var totalSize = firstHalf.Length + secondHalf.Length;
+
+        for (var i = 0; i < totalSize; i++)
+        {
+            var currentHalf = i % 2 == 0 ? firstHalf : secondHalf;
+            if (i / 2 < currentHalf.Length)
+            {
+                list[i] = currentHalf[i / 2];
+            }
+        }
+    }
+
+    public static void FaroShuffle<T>(this IList<T> list)
+    {
+        list.FaroShuffle(0, list.Count - 1);
+    }
+}
