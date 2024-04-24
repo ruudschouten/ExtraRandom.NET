fec196cc793d93ca021bb3c49f97d70cb0c15c3f
ruudschouten <ruud.schouten@outlook.com>
2024-04-24 11:44:08 +0200

    Added TheoryData for all shuffles, which are now tested in a single file

---
+++ ExtraShuffle.UnitTests/ShuffleTester.cs
@@ -0,0 +1,32 @@
+﻿using ExtraRandom.PRNG;
+using ExtraShuffle.TestHelper;
+using FluentAssertions;
+
+namespace ExtraShuffle.UnitTests;
+
+using static Playlist;
+
+public class Checker
+{
+    [Fact]
+    public void AllShuffles()
+    {
+        var unsorted = GetTestPlaylist();
+        var balanced = GetTestPlaylist();
+        var faro = GetTestPlaylist();
+        var fibonacci = GetTestPlaylist();
+        var fischerYates = GetTestPlaylist();
+        var random = new RomuTrio(500);
+        var grouping = new Func<Song, string>(song => song.Genre);
+
+        balanced.BalancedShuffle(random, grouping);
+        faro.FaroShuffle();
+        fibonacci.FibonacciHashingShuffle(random, grouping);
+        fischerYates.FischerYatesShuffle(random);
+
+        balanced.Should().NotContainInOrder(unsorted);
+        faro.Should().NotContainInOrder(unsorted);
+        fibonacci.Should().NotContainInOrder(unsorted);
+        fischerYates.Should().NotContainInOrder(unsorted);
+    }
+}
