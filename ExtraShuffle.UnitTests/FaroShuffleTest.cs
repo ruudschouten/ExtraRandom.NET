31dd7d31530d4297bce908302c315bdf7fc3face
ruudschouten <ruud.schouten@outlook.com>
2024-04-24 11:45:08 +0200

    Changed naming for FaroShuffle tests

--- ExtraShuffle.UnitTests/FaroShuffleTest.cs
+++ ExtraShuffle.UnitTests/FaroShuffleTest.cs
@@ -7,14 +7,14 @@ public class FaroShuffleTest
     private readonly List<int> _list = [1, 2, 3, 4, 5, 6, 7, 8];
 
     [Fact]
-    public void TestEven()
+    public void FaroShuffle_ShouldShuffle_WhenUsingAnEvenList()
     {
         _list.FaroShuffle();
         _list.Should().ContainInOrder(1, 5, 2, 6, 3, 7, 4, 8);
     }
 
     [Fact]
-    public void TestUneven()
+    public void FaroShuffle_ShouldShuffle_WhenUsingAnUnEvenList()
     {
         _list.Add(9);
         _list.FaroShuffle();
