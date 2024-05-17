b2fae5800c3e8d6ce8e496a41c07e181d11fbef2
ruudschouten <ruud.schouten@outlook.com>
2024-05-17 16:21:18 +0200

    Implemented Seiran according to its original implementation

--- src/ExtraRandom/PRNG/Seiran.cs
+++ src/ExtraRandom/PRNG/Seiran.cs
@@ -1,14 +1,11 @@
-using System.Numerics;
-
 namespace ExtraRandom.PRNG;
 
 /// <summary>
 /// Linear-feedback shift register-based pseudorandom number generators.
-/// Based on: https://github.com/Shiroechi/Litdex.Random/blob/main/Source/PRNG/Seiran.cs.
 /// </summary>
 /// <remarks>
 /// <para>
-/// Source: https://github.com/andanteyk/prng-seiran
+/// Source: https://github.com/andanteyk/prng-seiran/blob/master/seiran128.c
 /// </para>
 /// </remarks>
 public sealed class Seiran : Random64
@@ -24,13 +21,17 @@ public sealed class Seiran : Random64
     /// </para>
     /// </remarks>
     public Seiran(ulong baseSeed)
-        : this([baseSeed, baseSeed + 1]) { }
+        : this([baseSeed, baseSeed + 1])
+    {
+    }
 
     /// <summary>
     /// Initializes a new instance of the <see cref="Seiran"/> class.
     /// </summary>
     public Seiran(ulong seed1, ulong seed2)
-        : this([seed1, seed2]) { }
+        : this([seed1, seed2])
+    {
+    }
 
     /// <summary>
     /// Initializes a new instance of the <see cref="Seiran"/> class.
@@ -39,7 +40,17 @@ public sealed class Seiran : Random64
     private Seiran(ulong[] seed)
     {
         State = new ulong[2];
-        SetSeed(seed);
+        InitialiseState(seed[0]);
+    }
+
+    private void InitialiseState(ulong seed)
+    {
+        // You may initialize state by any method, unless state will be {0, 0}.
+        for (var i = 0; i < 2; i++)
+        {
+            seed = (seed * 6364136223846793005) + 1442695040888963407;
+            State[i] = seed;
+        }
     }
 
     /// <inheritdoc />
@@ -48,9 +59,13 @@ public sealed class Seiran : Random64
         var s0 = State[0];
         var s1 = State[1];
 
-        State[0] = s0 ^ BitOperations.RotateLeft(s1, 29);
+        var result = RotL((s0 + s1) * 9, 29) + s0;
+
+        State[0] = s0 ^ RotL(s1, 29);
         State[1] = s0 ^ s1 << 9;
 
-        return BitOperations.RotateLeft((State[0] + State[1]) * 9, 29) + State[0];
+        return result;
     }
+
+    private static ulong RotL(ulong x, int k) => (x << k) | (x >> (-k & 63));
 }
